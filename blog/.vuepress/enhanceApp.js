8321963be8b5acbdd2420d8e91a3ce38de3663c5
Steven Zhang <913835211@qq.com>
2021-09-16 01:53:00 -0400

    refactor: push new files

---
+++ blog/.vuepress/enhanceApp.js
@@ -0,0 +1,39 @@
+import OhVueIcon from "oh-vue-icons";
+
+import {
+  FaFortAwesome,
+  FaPaw,
+  FaTag,
+  FaSatelliteDish,
+  RiLinkM,
+  RiSearch2Line,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiFacebookBoxFill,
+  RiTwitterFill,
+  RiZhihuLine,
+  RiWeiboFill,
+  HiMail,
+  AiCv,
+  RiSpaceShipFill,
+  RiProfileLine
+} from "oh-vue-icons/icons";
+
+OhVueIcon.add(
+  FaFortAwesome,
+  FaPaw,
+  FaTag,
+  FaSatelliteDish,
+  RiLinkM,
+  RiSearch2Line,
+  RiGithubFill,
+  RiLinkedinBoxFill,
+  RiFacebookBoxFill,
+  RiTwitterFill,
+  RiZhihuLine,
+  RiWeiboFill,
+  HiMail,
+  AiCv,
+  RiSpaceShipFill,
+  RiProfileLine
+);
---
+++ blog/posts/2021-09-16-initialize.md
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Begin Again from New York"
+subtitle: ""
+author: "YuYan"
+date: 2021-09-16
+header_img: /img/in-post/2021-09-16/beauty-with-knifves.png
+catalog: true
+tags:
+  - 随笔
+---
+Sometimes, I hope that there will be a star always shining for me.<br/>
+Eventhough, so bad and weak am I.
+<!-- more -->
+
+## <s>少年</s> 青年宇言的烦恼
+
+(先占个坑，明天清醒了再写)
+
+
+```shell
+sudo reboot 
+```
